ca6a802765baccd0fcde7f4b4cad550b73be33fb
Stuart R. Jefferys <srjefferys@gmail.com>
2015-06-29 13:57:48 -0400

    Add testing infrastructure with testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(FusionExressionPlot)
+
+test_check("FusionExressionPlot")
---
+++ tests/testthat/test_0.r
@@ -0,0 +1,5 @@
+context("Test infrastructure")
+
+test_that( "Test that testing works", {
+   fail( "This should fail" )
+})
