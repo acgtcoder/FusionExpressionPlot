76d478e82b0231dee10478ec96883e65c5a9db60
Stuart R. Jefferys <srjefferys@gmail.com>
2015-07-02 22:13:23 -0400

    genomicToModelCoordinates - test different gapWdths.

--- tests/testthat/test_fusionExpressionPlot.r
+++ tests/testthat/test_fusionExpressionPlot.r
@@ -4,7 +4,9 @@ context("fusionExpressionPlot.R file.")
 #          100     200      1      100     10
 # Introns:   201-300,501-510,512-512,613-812
 #              100     10       1      200
-# Model:   1-100,201-400,501-501,602-701,802-811
+# Models:  1-100,  201-400,    501-501,    602-701,    802-811
+#          1-100,10101-10300,20301-20301,30302-30401,40402-40411
+#          1-100,102-301,303-303,305-404,406-415
 
 makeGrFull <- function() {
    gr <- GRanges(
@@ -31,11 +33,27 @@ test_that( 'genomicToModelCoordinates; coordinates <= first +exon are ok.', {
    got = genomicToModelCoordinates( c(1, 100, 101), makeGrPos(), 100 );
    want = c(0, 0, 1)
    expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(1, 100, 101), makeGrPos(), 10000 );
+   want = c(0, 0, 1)
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(1, 100, 101), makeGrPos(), 1 );
+   want = c(0, 0, 1)
+   expect_equal(want, got)
 })
 
 test_that( 'Test that genomicToModelCoordinates >= last +exon are ok.', {
    got = genomicToModelCoordinates( c(822, 823, 10000), makeGrPos(), 100 );
    want = c(811, 812, 812);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(822, 823, 10000), makeGrPos(), 10000 );
+   want = c(40411, 40412, 40412);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(822, 823, 10000), makeGrPos(), 1 );
+   want = c(415, 416, 416);
    expect_equal(want, got)
 })
 
@@ -45,9 +63,25 @@ test_that( 'genomicToModelCoordinates; coordinates in +exons are ok.', {
    want = c(1, 50, 100);
    expect_equal(want, got)
 
+   got = genomicToModelCoordinates( c(101, 150, 200), makeGrPos(), 10000 );
+   want = c(1, 50, 100);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(101, 150, 200), makeGrPos(), 1 );
+   want = c(1, 50, 100);
+   expect_equal(want, got)
+
    # internal exon/exon larger than gap
    got = genomicToModelCoordinates( c(301, 400, 500), makeGrPos(), 100 );
    want = c(201, 300, 400);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(301, 400, 500), makeGrPos(), 10000 );
+   want = c(10101, 10200, 10300);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(301, 400, 500), makeGrPos(), 1 );
+   want = c(102, 201, 301);
    expect_equal(want, got)
 
    # last exon/exon smaller than gap
@@ -55,9 +89,25 @@ test_that( 'genomicToModelCoordinates; coordinates in +exons are ok.', {
    want = c(802, 807, 811);
    expect_equal(want, got)
 
+   got = genomicToModelCoordinates( c(813, 818, 822), makeGrPos(), 10000 );
+   want = c(40402, 40407, 40411);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(813, 818, 822), makeGrPos(), 1 );
+   want = c(406, 411, 415);
+   expect_equal(want, got)
+
    # exon of size 1/single value works
    got = genomicToModelCoordinates( 511, makeGrPos(), 100 );
    want = 501;
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( 511, makeGrPos(), 10000 );
+   want = 20301;
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( 511, makeGrPos(), 1 );
+   want = 303;
    expect_equal(want, got)
 })
 
@@ -67,14 +117,26 @@ test_that( 'Test that genomicToModelCoordinates in +gaps are ok.', {
    want = c(101, 150, 200);
    expect_equal(want, got)
 
-   # internal gap ( < gapWidth)
+   # internal gap
    got = genomicToModelCoordinates( c(501, 502, 505, 510), makeGrPos(), 100 );
    want = c(405, 415, 445, 495);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(501, 502, 505, 510), makeGrPos(), 1 );
+   want = c(302, 302, 302, 302);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(501, 502, 505, 510), makeGrPos(), 10000 );
+   want = c(10800, 11800, 14800, 19800);
    expect_equal(want, got)
 
    # last gap ( > gapWidth)
    got = genomicToModelCoordinates( c(613, 712, 812), makeGrPos(), 100 );
    want = c(702, 751, 801);
+   expect_equal(want, got)
+
+   got = genomicToModelCoordinates( c(613, 712, 812), makeGrPos(), 1 );
+   want = c(405, 405, 405);
    expect_equal(want, got)
 
    # borders, size 1 intron
@@ -199,6 +261,53 @@ test_that( 'genomicToModelCoordinates; extremes ok, +model?', {
    got = genomicToModelCoordinates(
           c(200200, 200201, 200202), gr, 100 );
    want = c(100200, 100201, 100201);
+   expect_equal(want, got)
+
+})
+
+test_that( 'genomicToModelCoordinates; extremes ok, -model?', {
+
+   # Exons: 101-100100,200101-200200
+   #          100000        100
+   # Introns:   100101-200100
+   #                100000
+
+   # Model:   1-100,201-100200
+   gr <- GRanges(
+      ranges= IRanges( start= c( 101, 200101 ),
+                       end=   c( 100100, 200200 )),
+      seqnames= c( "chr1" ),
+      strand= c( "-" )
+   );
+
+   # Before
+   got = genomicToModelCoordinates(
+          c(     1,    100,    101), gr, 100 );
+   want = c(100201, 100201, 100200);
+   expect_equal(want, got)
+
+   # Exon 1
+   got = genomicToModelCoordinates(
+          c(   100,    101,     102,  50100, 100099, 100100, 100101), gr, 100 );
+   want = c(100201, 100200,  100199,  50201,  202, 201, 200);
+   expect_equal(want, got)
+
+   # Intron 1
+   got = genomicToModelCoordinates(
+          c(100100, 100101, 150101, 200100, 200101), gr, 100 );
+   want = c(201, 200, 150, 101, 100);
+   expect_equal(want, got)
+
+   # Exon 2
+   got = genomicToModelCoordinates(
+       c(200100, 200101, 200102, 200199, 200200, 200201), gr, 100 );
+   want = c(101,    100,     99,      2,      1,      0);
+   expect_equal(want, got)
+
+   # After
+   got = genomicToModelCoordinates(
+          c(200200, 200201, 200202), gr, 100 );
+   want = c(1,           0,      0);
    expect_equal(want, got)
 
 })
