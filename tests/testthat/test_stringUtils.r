42ce52eacdbcd791e663e59591f71cc4b699ccd0
Stuart R. Jefferys <srjefferys@gmail.com>
2015-07-17 00:50:12 -0400

    Convert stringUtils test file to use describe() + it() testing

--- tests/testthat/test_stringUtils.r
+++ tests/testthat/test_stringUtils.r
@@ -3,66 +3,74 @@ context("Unit testing functions in the stringUtils.R file.")
 #
 # regexprNamedMatches
 #
+describe( 'regexprNamedMatches()', {
+   it( "Retrieves multi-group matched text across a vector of strings", {
+      regExp <- "(?<key>.+?)\\s*=\\s*(?<value>.+)"
+      data <- c('name = Stuart R. Jefferys', 'email=srj@unc.edu')
 
-test_that( "regexprNamedMatches returns correct results", {
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data)
+      want<-do.call(cbind, list(key=c('name', 'email'), value=c('Stuart R. Jefferys', 'srj@unc.edu')))
+      expect_equal(got, want)
+   })
 
-   # Normal matching
-   regExp <- "(?<key>.+?)\\s*=\\s*(?<value>.+)"
-   data <- c('name = Stuart R. Jefferys', 'email=srj@unc.edu')
+   it( "Retrieves mutli-group matched text from a single string", {
+      regExp <- "(?<key>.+?)\\s*=\\s*(?<value>.+)"
+      data <- c('name = Stuart R. Jefferys')
 
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data)
-   want<-do.call(cbind, list(key=c('name', 'email'), value=c('Stuart R. Jefferys', 'srj@unc.edu')))
-   expect_equal(got, want)
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data)
+      want<-do.call(cbind, list(key=c('name'), value=c('Stuart R. Jefferys')))
+      expect_equal(got, want)
+   })
 
-   # If only one string
-   regExp <- "(?<key>.+?)\\s*=\\s*(?<value>.+)"
-   data <- c('name = Stuart R. Jefferys')
+   it( "Retrieves single-group matched text across a vector of strings", {
+      regExp <- ".*?(?<num>\\d+).*"
+      data <- c('Embedded 123 number', '0234')
 
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data)
-   want<-do.call(cbind, list(key=c('name'), value=c('Stuart R. Jefferys')))
-   expect_equal(got, want)
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data)
+      want<-do.call(cbind, list(num=c('123', '0234')))
+      expect_equal(got,want)
+   })
 
-   # If only one capture group
-   regExp <- ".*?(?<num>\\d+).*"
-   data <- c('Embedded 123 number', '0234')
+   it( "Retrieves empty text string by default when doesn't match", {
+      regExp <- "(?<key>.+?)\\s*=\\s*(?<value>\\d+)"
+      data <- c('id = 0123', 'email=srj@unc.edu')
 
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data)
-   want<-do.call(cbind, list(num=c('123', '0234')))
-   expect_equal(got,want)
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data)
+      want<-do.call(cbind, list(key=c('id', ''), value=c('0123', '')))
+      expect_equal(got, want)
+   })
 
-   # If doesn't match?
-   regExp <- "(?<key>.+?)\\s*=\\s*(?<value>\\d+)"
-   data <- c('id = 0123', 'email=srj@unc.edu')
+   it( "Retrieves NA if matches nothing and use.na is specified", {
+      regExp <- "(?<key>.+?)\\s*=\\s*(?<value>\\d+)"
+      data <- c('id = 0123', 'email=srj@unc.edu')
 
-   # Want ''
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data)
-   want<-do.call(cbind, list(key=c('id', ''), value=c('0123', '')))
-   expect_equal(got, want)
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data, use.na=TRUE)
+      want<-do.call(cbind, list(key=c('id', NA), value=c('0123', NA)))
+      expect_equal(got, want)
+   })
 
-   # Want NAs
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data, use.na=TRUE)
-   want<-do.call(cbind, list(key=c('id', NA), value=c('0123', NA)))
-   expect_equal(got, want)
+   it( "Retrieves empty text string by default if matches but captures no text", {
+      regExp <- "(?<key>.+?)\\s*=\\s*(?<value>\\d*)"
+      data <- c('id = 0123', 'email=srj@unc.edu')
 
-   # If matches with substring empty?
-   regExp <- "(?<key>.+?)\\s*=\\s*(?<value>\\d*)"
-   data <- c('id = 0123', 'email=srj@unc.edu')
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data)
+      want<-do.call(cbind, list(key=c('id', 'email'), value=c('0123', '')))
+      expect_equal(got, want)
+   })
+   it( "Still retrieves empty text string if matches but captures no text when use.na is specified", {
+      regExp <- "(?<key>.+?)\\s*=\\s*(?<value>\\d*)"
+      data <- c('id = 0123', 'email=srj@unc.edu')
 
-   # Want ''
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data)
-   want<-do.call(cbind, list(key=c('id', 'email'), value=c('0123', '')))
-   expect_equal(got, want)
-
-   # Still want '', not NAs
-   matchResults <- regexpr(regExp, data, perl= TRUE)
-   got<-regexprNamedMatches(matchResults, data, use.na=TRUE)
-   want<-do.call(cbind, list(key=c('id', 'email'), value=c('0123', '')))
-   expect_equal(got, want)
-
+      # Still want '', not NAs
+      matchResults <- regexpr(regExp, data, perl= TRUE)
+      got<-regexprNamedMatches(matchResults, data, use.na=TRUE)
+      want<-do.call(cbind, list(key=c('id', 'email'), value=c('0123', '')))
+      expect_equal(got, want)
+   })
 })
